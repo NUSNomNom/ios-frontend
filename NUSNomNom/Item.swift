d4e651d82646b4e698389f685e063619ec15c50e
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-05-05 15:20:46 +0800

    Initial commit

---
+++ NUSNomNom/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NUSNomNom
+//
+//  Created by Nutabi on 5/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
