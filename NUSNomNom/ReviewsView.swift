9af89a11bc3eb1881b8d7bb98f66ffbc379971d9
kevanchng <kevanchng123@gmail.com>
2025-05-28 15:06:40 +0800

    Add Main View
    added main view with tabs to nvigate to search, reviews, navigate and
    settings view

---
+++ NUSNomNom/ReviewsView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewsView.swift
+//  NUSNomNom
+//
+//  Created by Kevan Chng on 28/5/25.
+//
+
+import SwiftUI
+
+struct ReviewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewsView()
+}
