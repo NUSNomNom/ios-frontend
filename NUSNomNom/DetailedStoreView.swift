d0f3db51ef12171681dfa19004af4e573d316d42
kevanchng <kevanchng123@gmail.com>
2025-05-28 21:15:10 +0800

    Add Detailed Store View
    added view to show store name, description, menu when clicked on
    specific store from search view

---
+++ NUSNomNom/DetailedStoreView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedStoreView.swift
+//  NUSNomNom
+//
+//  Created by Kevan Chng on 28/5/25.
+//
+
+import SwiftUI
+
+struct DetailedStoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailedStoreView()
+}
