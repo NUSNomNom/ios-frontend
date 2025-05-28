68315acfd892c24f6b0ae6a955b55d27d2815f81
kevanchng <kevanchng123@gmail.com>
2025-05-28 21:28:30 +0800

    Add Search View Model
    a search view model is added to track which store is seleceted in the
    search view and control whether the detailed store view should be shown

---
+++ NUSNomNom/SearchViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewModel.swift
+//  NUSNomNom
+//
+//  Created by Kevan Chng on 28/5/25.
+//
+
+import SwiftUI
+
+struct SearchViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchViewModel()
+}
