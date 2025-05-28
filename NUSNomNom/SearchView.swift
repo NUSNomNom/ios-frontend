bcd8aeff1b784fe8fd7a41e9cffbac8bb5ec6916
kevanchng <kevanchng123@gmail.com>
2025-05-28 18:55:04 +0800

    Add Non-functional Search Bar
    added a non-functional search bar which will become functional later on

--- NUSNomNom/SearchView.swift
+++ NUSNomNom/SearchView.swift
@@ -8,40 +8,54 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var dummySearchText: String = ""
     
     var body: some View {
         
-        
-        NavigationStack {
-            List(MockStoreData.stores) {
-                store in
-                VStack(alignment: .leading, spacing: 6) {
-                    HStack {
-                        Text(store.name)
-                            .font(.headline)
-                        Spacer()
-                        Text(store.isOpen ? "Open" : "Closed")
-                            .font(.caption)
-                            .foregroundColor(store.isOpen ? .green : .red)
-                    }
-                    
-                    Text(store.cuisine)
-                        .font(.subheadline)
-                        .fontWeight(.regular)
-                        .foregroundColor(.black)
-                    
-                    Text(store.description)
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                        .lineLimit(3)
-                }
-                .padding(.vertical, 4)
+        VStack {
+            
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                TextField("Search food stalls...", text: $dummySearchText)
             }
-            .navigationTitle("All Food Stalls")
+            .padding(10)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding()
+            
+            
+            NavigationStack {
+                List(MockStoreData.stores) {
+                    store in
+                    VStack(alignment: .leading, spacing: 6) {
+                        HStack {
+                            Text(store.name)
+                                .font(.headline)
+                            Spacer()
+                            Text(store.isOpen ? "Open" : "Closed")
+                                .font(.caption)
+                                .foregroundColor(store.isOpen ? .green : .red)
+                        }
+                        
+                        Text(store.cuisine)
+                            .font(.subheadline)
+                            .fontWeight(.regular)
+                            .foregroundColor(.black)
+                        
+                        Text(store.description)
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                            .lineLimit(3)
+                    }
+                    .padding(.vertical, 4)
+                }
+                .navigationTitle("All Food Stalls")
+            }
+            
         }
     }
 }
-
 #Preview {
     SearchView()
 }
