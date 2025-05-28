2a705620796fa5fd48d06f88c37ae730c541fcd4
kevanchng <kevanchng123@gmail.com>
2025-05-28 21:31:29 +0800

    Add detailed store view function
    added a navigation link to view detailed store for each store when
    selected on search view

--- NUSNomNom/DetailedStoreView.swift
+++ NUSNomNom/DetailedStoreView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct DetailedStoreView: View {
+    
+    var store:Store
+    @Binding var isShowingDetailedView: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack (spacing: 20) {
+            
+            Spacer()
+            
+            Text(store.name)
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Text(store.description)
+                .font(.title2)
+                .fontWeight(.medium)
+                .padding()
+            
+            Text("placeholder for menu")
+                .font(.title2)
+                .fontWeight(.medium)
+                .padding()
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    DetailedStoreView()
+    DetailedStoreView(store: MockStoreData.sampleStore, isShowingDetailedView: .constant(false))
 }
