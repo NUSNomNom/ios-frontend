ea93fde5e11ba0e6e41bac4e24872ef1a8c31245
kevanchng <kevanchng123@gmail.com>
2025-05-28 21:48:27 +0800

    Add Setting View
    added login feature to setting view that shows profile after sucessful
    login

--- NUSNomNom/SettingView.swift
+++ NUSNomNom/SettingView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct SettingView: View {
+    @AppStorage("isLoggedIn") private var isLoggedIn: Bool = false
+    @State private var isShowingLogin = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section(header: Text("Account")) {
+                    if isLoggedIn {
+                        HStack {
+                            Image(systemName: "person.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(.blue)
+                            VStack(alignment: .leading) {
+                                Text("placeholder name")
+                                    .font(.headline)
+                                Text("student@u.nus.edu")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                        
+                        Button("Logout") {
+                            isLoggedIn = false
+                        }
+                        .foregroundColor(.red)
+                    } else {
+                        Button("Login") {
+                            isShowingLogin = true
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Settings")
+            .sheet(isPresented: $isShowingLogin) {
+                LoginView()
+            }
+        }
     }
 }
+
 
 #Preview {
     SettingView()
