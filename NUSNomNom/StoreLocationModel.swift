3fa35b0d5ba084f3436899adaffc3509cef94af8
kevanchng <kevanchng123@gmail.com>
2025-06-09 15:47:15 +0800

    Added Store Location Model
    To store each location as its separate entity for each location so as to
    no intefere store

--- NUSNomNom/StoreLocationModel.swift
+++ NUSNomNom/StoreLocationModel.swift
@@ -5,4 +5,12 @@
 //  Created by Kevan Chng on 9/6/25.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
+
+struct StoreLocation: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
