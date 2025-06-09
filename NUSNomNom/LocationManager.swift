d231396158d9fa2886195ac6fc3314f263aae80f
kevanchng <kevanchng123@gmail.com>
2025-06-09 15:45:20 +0800

    Created Location Manager to track current location
    tracks user's current location for navigation view to use current
    location

--- NUSNomNom/LocationManager.swift
+++ NUSNomNom/LocationManager.swift
@@ -5,4 +5,32 @@
 //  Created by Kevan Chng on 9/6/25.
 //
 
-import Foundation
+import CoreLocation
+import Combine
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    
+    @Published var userLocation: CLLocationCoordinate2D?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let firstLocation = locations.first else { return }
+        DispatchQueue.main.async {
+            self.userLocation = firstLocation.coordinate
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location error: \(error.localizedDescription)")
+    }
+}
+
+
