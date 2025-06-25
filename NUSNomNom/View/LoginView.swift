a5dc99f700114021f42ecab00eb455fcfdc3f572
kevanchng <kevanchng123@gmail.com>
2025-06-25 15:49:21 +0800

    Updated UI for Login, Register, All and Settings View
    Chnaged UI design for the stated views and added previews

--- NUSNomNom/View/LoginView.swift
+++ NUSNomNom/View/LoginView.swift
@@ -19,67 +19,87 @@ struct LoginView: View {
     
     @EnvironmentObject var auth: AuthManager
     
+    @Environment(\.dismiss) private var dismiss
+    
     @Binding var loginSuccess: Bool
 
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.nusBlue, .nusOrange]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .ignoresSafeArea()
+        
+        VStack(spacing: 20) {
             
-            VStack(spacing: 20) {
-                Text("Welcome")
-                    .foregroundStyle(.white)
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                
-                Image("nusLogo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 350, height: 150)
-                
-                TextField("Email", text: $inputEmail)
-                    .autocapitalization(.none)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(12)
-                
-                SecureField("Password", text: $inputPassword)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(12)
-                
-                Button("Login") {
-                    Task {
-                        do {
-                            try await auth.login(as: inputEmail, with: inputPassword)
-                            loginSuccess = true
-                        } catch {
-                            showLoginErrorAlert = true
-                            loginErrorMessage = error.localizedDescription
-                        }
+            HStack {
+                Spacer()
+                Button("Cancel") {
+                    dismiss()
+                }
+                .foregroundColor(.blue)
+                .fontWeight(.bold)
+                .padding(.trailing)
+            }
+            .padding(.bottom, 40)
+            
+            Text("Welcome to")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Image("nusNomNomLongLogo")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 250, height: 80)
+                .clipped()
+            
+            Text("Sign in with email!")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Text("Sign in to write reviews for your favourite stores, use NUS NomNom on multiple devices and more!")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+            
+            TextField("Email", text: $inputEmail)
+                .autocapitalization(.none)
+                .padding()
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(12)
+            
+            SecureField("Password", text: $inputPassword)
+                .padding()
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(12)
+            
+            Button("Login") {
+                Task {
+                    do {
+                        try await auth.login(as: inputEmail, with: inputPassword)
+                        loginSuccess = true
+                    } catch {
+                        showLoginErrorAlert = true
+                        loginErrorMessage = error.localizedDescription
                     }
                 }
-                .foregroundStyle(.white)
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(12)
-                .disabled(inputEmail.isEmpty || inputPassword.isEmpty)
-                
-                
-                Button("Register") {
-                    showRegisterView = true
-                }
-                .foregroundStyle(.white)
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.orange)
-                .cornerRadius(12)
-                
             }
+            .foregroundStyle(.white)
+            .fontWeight(.bold)
+            .frame(maxWidth: .infinity)
             .padding()
+            .background(Color.blue)
+            .cornerRadius(12)
+            .disabled(inputEmail.isEmpty || inputPassword.isEmpty)
+            
+            
+            Button("Register") {
+                showRegisterView = true
+            }
+            .foregroundStyle(.white)
+            .fontWeight(.bold)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.orange)
+            .cornerRadius(12)
+            
         }
+        .padding()
         .sheet(isPresented: $showRegisterView) {
             RegisterView(registerSuccess: $showRegisterSuccessAlert)
                 .environmentObject(auth)
@@ -98,5 +118,11 @@ struct LoginView: View {
         } message: {
             Text(loginErrorMessage)
         }
+        Spacer()
     }
+}
+
+#Preview {
+    LoginView(loginSuccess: .constant(false))
+        .environmentObject(AuthManager.shared)
 }
